c40d56f2a9dc06c455b3922fdd0c1094e0b38ce7
ercelserhat <ercelserhat@gmail.com>
2022-12-18 22:57:44 +0300

    Auto layout four color example

---
+++ AutoLayout-FourColorExample/AutoLayout-FourColorExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout-FourColorExample
+//
+//  Created by Serhat on 18.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
