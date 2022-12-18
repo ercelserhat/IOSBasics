abeb9607997bfc2d9f49ad4b1f5be7831e8e6b40
ercelserhat <ercelserhat@gmail.com>
2022-12-19 00:57:03 +0300

    Auto layout - Recipe app page example

---
+++ AutoLayout-RecipeExample/AutoLayout-RecipeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout-RecipeExample
+//
+//  Created by Serhat on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutoLayout-RecipeExample/AutoLayout-RecipeExampleUITests/AutoLayout_RecipeExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AutoLayout_RecipeExampleUITestsLaunchTests.swift
+//  AutoLayout-RecipeExampleUITests
+//
+//  Created by Serhat on 19.12.2022.
+//
+
+import XCTest
+
+final class AutoLayout_RecipeExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
