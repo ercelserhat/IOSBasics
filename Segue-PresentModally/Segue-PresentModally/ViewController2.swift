39dcbf5a637a0684ed9f8b1919955001f49c8446
ercelserhat <ercelserhat@gmail.com>
2022-12-20 00:53:40 +0300

    Seque - Present modally on storyboard

---
+++ Segue-PresentModally/Segue-PresentModally/ViewController2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  Segue-PresentModally
+//
+//  Created by Serhat on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Segue-PresentModally/ViewController3.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController3.swift
+//  Segue-PresentModally
+//
+//  Created by Serhat on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
