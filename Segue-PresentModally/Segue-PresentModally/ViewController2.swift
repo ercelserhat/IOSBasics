7c8038af86f6a463bcccd0156161117a759d279e
ercelserhat <ercelserhat@gmail.com>
2022-12-20 22:08:05 +0300

    Present modally - dismiss()

--- Segue-PresentModally/Segue-PresentModally/ViewController2.swift
+++ Segue-PresentModally/Segue-PresentModally/ViewController2.swift
@@ -15,4 +15,7 @@ class ViewController2: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tiklaDismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
