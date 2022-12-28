8a748bc00b30f7ab62b463d1f07638a062da1b27
ercelserhat <ercelserhat@gmail.com>
2022-12-29 01:23:35 +0300

    Tab Bar - Badge

--- TabBar/TabBar/ViewController.swift
+++ TabBar/TabBar/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var profilSayac = 0
+    var isSayac = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func profilArttir(_ sender: Any) {
+        if let tabItems = tabBarController?.tabBar.items{
+            let profilTabBarItem = tabItems[0]
+            profilSayac += 1
+            profilTabBarItem.badgeColor = UIColor.purple
+            profilTabBarItem.badgeValue = String(profilSayac)
+        }
+    }
+    
+    @IBAction func isArttir(_ sender: Any) {
+        if let tabItems = tabBarController?.tabBar.items{
+            let isBarItem = tabItems[1]
+            isSayac += 1
+            isBarItem.badgeColor = UIColor.red
+            isBarItem.badgeValue = String(isSayac)
+        }
+    }
 }
 
