01a7c0b1edb5c47d6c760cb7acee28eda7b0fec7
ercelserhat <ercelserhat@gmail.com>
2022-12-28 23:26:30 +0300

    Tab Bar

---
+++ TabBar/TabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBar
+//
+//  Created by Serhat on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
