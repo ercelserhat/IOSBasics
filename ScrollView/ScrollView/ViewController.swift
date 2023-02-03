b93eda6156599bab0e30dc527e0915ed206de606
ercelserhat <ercelserhat@gmail.com>
2023-02-03 21:58:38 +0300

    Widgets - ScrollView

---
+++ ScrollView/ScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Serhat on 3.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
