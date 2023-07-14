2d1d3f54c31921da41c7ee59aba47f3d19106613
ercelserhat <ercelserhat@gmail.com>
2023-07-14 16:15:04 +0300

    Read function

--- SQLite/SQLite/Kisilerdao.swift
+++ SQLite/SQLite/Kisilerdao.swift
@@ -27,4 +27,20 @@ class Kisilerdao{
         
         db?.close()
     }
+    
+    func tumKisileriAl() -> [Kisiler]{
+        var liste = [Kisiler]()
+        db?.open()
+        do{
+            let rs = try db!.executeQuery("SELECT * FROM kisiler", values: nil)
+            while rs.next(){
+                let kisi = Kisiler(kisi_id: Int(rs.string(forColumn: "kisi_id")!)!, kisi_ad: rs.string(forColumn: "kisi_ad")!, kisi_yas: Int(rs.string(forColumn: "kisi_yas")!)!)
+                liste.append(kisi)
+            }
+        }catch{
+            print(error.localizedDescription)
+        }
+        db?.close()
+        return liste
+    }
 }
