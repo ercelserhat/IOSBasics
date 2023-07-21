e098163bc685fd06824192b02a3470c1d54c19c5
ercelserhat <ercelserhat@gmail.com>
2023-07-21 15:13:00 +0300

    Adding Action to Notification

--- Notifications/Notifications/ViewController.swift
+++ Notifications/Notifications/ViewController.swift
@@ -29,22 +29,30 @@ class ViewController: UIViewController {
 
     @IBAction func bildirimOlustur(_ sender: Any) {
         if izinKontrol{
+            let evet = UNNotificationAction(identifier: "evet", title: "Evet", options: .foreground)
+            let hayir = UNNotificationAction(identifier: "hayir", title: "Hayır", options: .foreground)
+            let sil = UNNotificationAction(identifier: "sil", title: "Sil", options: .destructive)
+            let kategori = UNNotificationCategory(identifier: "kategori", actions: [evet, hayir, sil], intentIdentifiers: [], options: [])
+            UNUserNotificationCenter.current().setNotificationCategories([kategori])
+            
             let icerik = UNMutableNotificationContent()
             icerik.title = "Başlık"
             icerik.subtitle = "Alt Başlık"
-            icerik.body = "Mesaj"
+            icerik.body = "5, 4'ten büyük mü?"
             icerik.badge = 1
             icerik.sound = UNNotificationSound.default
+            icerik.categoryIdentifier = "kategori"
             
-            //let tetikleme = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
-            var date = DateComponents()
+            let tetikleme = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+            /*var date = DateComponents()
             date.minute = 30
             date.hour = 8
             date.day = 20
             date.month = 4
             let tetikleme2 = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+             */
             
-            let bildirimIstegi = UNNotificationRequest(identifier: "Notifications", content: icerik, trigger: tetikleme2)
+            let bildirimIstegi = UNNotificationRequest(identifier: "Notifications", content: icerik, trigger: tetikleme)
             
             UNUserNotificationCenter.current().add(bildirimIstegi, withCompletionHandler: nil)
         }
@@ -55,5 +63,17 @@ class ViewController: UIViewController {
 extension ViewController: UNUserNotificationCenterDelegate{
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .sound, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        if response.actionIdentifier == "evet"{
+            print("Doğru cevap")
+        }
+        if response.actionIdentifier == "hayir"{
+            print("Yanlış cevap")
+        }
+        if response.actionIdentifier == "sil"{
+            print("Cevap verilmedi.")
+        }
     }
 }
