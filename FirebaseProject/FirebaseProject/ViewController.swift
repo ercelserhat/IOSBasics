f469ac721e306633c4ba28961835e454e22ed659
ercelserhat <ercelserhat@gmail.com>
2023-07-28 23:01:09 +0300

    Firebase SELECT

--- FirebaseProject/FirebaseProject/ViewController.swift
+++ FirebaseProject/FirebaseProject/ViewController.swift
@@ -17,7 +17,11 @@ class ViewController: UIViewController {
         ref = Database.database().reference()
         //kisiEkle()
         //kisiSil()
-        kisiGuncelle()
+        //kisiGuncelle()
+        //tumKisiler()
+        //kisiEqualSorgu()
+        //kisiLimitSorgu()
+        kisiDegerAraligiSorgu()
     }
     
     func kisiEkle(){
@@ -34,6 +38,85 @@ class ViewController: UIViewController {
     func kisiGuncelle(){
         let dict: [String : Any] = ["kisi_ad" : "YENİ AD", "kisi_yas" : 50]
         ref?.child("kisiler").child("-NaO72sDE3QVq76HREMC").updateChildValues(dict)
+    }
+    
+    func tumKisiler(){
+        ref?.child("kisiler").observe(.value, with: { snapshot in
+            if let gelenVeriButunu = snapshot.value as? [String : AnyObject]{
+                for gelenSatirVerisi in gelenVeriButunu{
+                    if let sozluk = gelenSatirVerisi.value as? NSDictionary{
+                        let key = gelenSatirVerisi.key
+                        let kisi_ad = sozluk["kisi_ad"] as? String ?? ""
+                        let kisi_yas = sozluk["kisi_yas"] as? Int ?? 0
+                        
+                        print("Key: \(key)")
+                        print("Ad: \(kisi_ad)")
+                        print("Yaş: \(kisi_yas)")
+                        print("----------------------")
+                    }
+                }
+            }
+        })
+    }
+    
+    func kisiEqualSorgu(){
+        let sorgu = ref?.child("kisiler").queryOrdered(byChild: "kisi_ad").queryEqual(toValue: "Serhat")
+        sorgu?.observe(.value, with: { snapshot in
+            if let gelenVeriButunu = snapshot.value as? [String : AnyObject]{
+                for gelenSatirVerisi in gelenVeriButunu{
+                    if let sozluk = gelenSatirVerisi.value as? NSDictionary{
+                        let key = gelenSatirVerisi.key
+                        let kisi_ad = sozluk["kisi_ad"] as? String ?? ""
+                        let kisi_yas = sozluk["kisi_yas"] as? Int ?? 0
+                        
+                        print("Key: \(key)")
+                        print("Ad: \(kisi_ad)")
+                        print("Yaş: \(kisi_yas)")
+                        print("----------------------")
+                    }
+                }
+            }
+        })
+    }
+    
+    func kisiLimitSorgu(){
+        let sorgu = ref?.child("kisiler").queryLimited(toFirst: 1)
+        sorgu?.observe(.value, with: { snapshot in
+            if let gelenVeriButunu = snapshot.value as? [String : AnyObject]{
+                for gelenSatirVerisi in gelenVeriButunu{
+                    if let sozluk = gelenSatirVerisi.value as? NSDictionary{
+                        let key = gelenSatirVerisi.key
+                        let kisi_ad = sozluk["kisi_ad"] as? String ?? ""
+                        let kisi_yas = sozluk["kisi_yas"] as? Int ?? 0
+                        
+                        print("Key: \(key)")
+                        print("Ad: \(kisi_ad)")
+                        print("Yaş: \(kisi_yas)")
+                        print("----------------------")
+                    }
+                }
+            }
+        })
+    }
+    
+    func kisiDegerAraligiSorgu(){
+        let sorgu = ref?.child("kisiler").queryOrdered(byChild: "kisi_yas").queryStarting(atValue: 20).queryEnding(atValue: 40)
+        sorgu?.observe(.value, with: { snapshot in
+            if let gelenVeriButunu = snapshot.value as? [String : AnyObject]{
+                for gelenSatirVerisi in gelenVeriButunu{
+                    if let sozluk = gelenSatirVerisi.value as? NSDictionary{
+                        let key = gelenSatirVerisi.key
+                        let kisi_ad = sozluk["kisi_ad"] as? String ?? ""
+                        let kisi_yas = sozluk["kisi_yas"] as? Int ?? 0
+                        
+                        print("Key: \(key)")
+                        print("Ad: \(kisi_ad)")
+                        print("Yaş: \(kisi_yas)")
+                        print("----------------------")
+                    }
+                }
+            }
+        })
     }
 }
 
