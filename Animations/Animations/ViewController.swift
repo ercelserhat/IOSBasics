b9ded045710b2e588652953a239091ecd6a74adb
ercelserhat <ercelserhat@gmail.com>
2023-08-24 16:33:45 +0300

    Animations - FAB Button

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -11,8 +11,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var animationLabel: UILabel!
     
+    @IBOutlet weak var fabMain: UIButton!
+    @IBOutlet weak var fabBirinci: UIButton!
+    @IBOutlet weak var fabIkinci: UIButton!
+    
+    var fabDurum = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fabMain.layer.cornerRadius = fabMain.frame.size.width / 2
+        fabBirinci.layer.cornerRadius = fabBirinci.frame.size.width / 2
+        fabIkinci.layer.cornerRadius = fabIkinci.frame.size.width / 2
+        
+        self.fabBirinci.alpha = 0
+        self.fabIkinci.alpha = 0
+        self.fabBirinci.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        self.fabIkinci.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
     }
 
     @IBAction func alphaAnimation(_ sender: Any) {
@@ -73,4 +88,35 @@ class ViewController: UIViewController {
             })
         })
     }
+    
+    @IBAction func fabMainTikla(_ sender: Any) {
+        UIView.animate(withDuration: 0.5, animations: {
+            if self.fabDurum{
+                self.fabBirinci.alpha = 0
+                self.fabIkinci.alpha = 0
+                
+                self.fabMain.transform = CGAffineTransform(rotationAngle: 0 * .pi / 180)
+                self.fabBirinci.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                self.fabIkinci.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                
+                self.fabDurum = false
+            }else{
+                self.fabBirinci.alpha = 1
+                self.fabIkinci.alpha = 1
+                
+                self.fabMain.transform = CGAffineTransform(rotationAngle: 45 * .pi / 180)
+                self.fabBirinci.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.fabIkinci.transform = CGAffineTransform(scaleX: 1, y: 1)
+                
+                self.fabDurum = true
+            }
+        }, completion: nil)
+    }
+    
+    @IBAction func fabBirinciTikla(_ sender: Any) {
+    }
+    
+    @IBAction func fabIkinciTikla(_ sender: Any) {
+    }
+    
 }
